023c404d29c46d83b1329dadb279a8bef7a89b47
shamoon <4887959+shamoon@users.noreply.github.com>
2026-07-28 13:16:23 -0700

    Backend stuff: add text choice icon field and migration

--- src/documents/serialisers.py
+++ src/documents/serialisers.py
@@ -1389,6 +1389,7 @@ class SavedViewSerializer(OwnedObjectSerializer):
         fields = [
             "id",
             "name",
+            "icon",
             "sort_field",
             "sort_reverse",
             "filter_rules",
